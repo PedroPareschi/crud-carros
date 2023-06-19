48a4c72c7aef3e460b99591514a1d803dd71b09b
pedropareschi <pedropareschi15@gmail.com>
2023-06-19 00:40:55 -0300

    Cadastro com senha encriptografada

--- src/main/java/com/pedropareschi/carros/config/SecurityConfig.java
+++ src/main/java/com/pedropareschi/carros/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -15,12 +16,14 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private static final String[] PUBLIC_MATCHERS = {"/carros"};
+    private static final String[] PUBLIC_MATCHERS_GET = {"/carros"};
+    private static final String[] PUBLIC_MATCHERS_POST = {"/clientes"};
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable();
-        http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS).permitAll()
+        http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+                .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
                 .anyRequest().authenticated();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
@@ -30,5 +33,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
         return source;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
--- src/main/java/com/pedropareschi/carros/service/ClienteService.java
+++ src/main/java/com/pedropareschi/carros/service/ClienteService.java
@@ -3,14 +3,17 @@ package com.pedropareschi.carros.service;
 import com.pedropareschi.carros.entity.Cliente;
 import com.pedropareschi.carros.repository.ClienteRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class ClienteService {
     private ClienteRepository repository;
+    private BCryptPasswordEncoder encoder;
 
     public Cliente cadastrarCliente(Cliente cliente){
+        cliente.setSenha(encoder.encode(cliente.getSenha()));
         return repository.save(cliente);
     }
 
